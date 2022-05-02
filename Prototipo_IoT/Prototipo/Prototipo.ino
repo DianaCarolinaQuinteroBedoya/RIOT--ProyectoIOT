8e669445b8ef1820ebb4738eb436cbb3ab38edbe
Oscar Rodriguez <oscar.rodriguez02@correo.usa.edu.co>
2022-05-01 19:48:51 -0500

    Se definieron las entradas y salidas del circuito

--- Prototipo_IoT/Prototipo/Prototipo.ino
+++ Prototipo_IoT/Prototipo/Prototipo.ino
@@ -10,3 +10,12 @@ int verde=9; //se define el led verde con el pin 10
 int humedad=A0; //se define la humedad al analogico 0
 float tempC; // se define variable para almacenar el valor obtenido de temperatura
 int temp = A1; // se define variable del pin de entrada del sensor (A1)
+
+void setup()
+{
+  Serial.begin(9600);// Configuramos el puerto serial a 9600 bps
+  pinMode(humedad, INPUT);//Se define la variable humedad como entrada
+  pinMode(rojo,OUTPUT);//Se define la variable rojo como salida
+  pinMode(verde,OUTPUT);//se define la variable verde como salida
+  aspersor.attach(4);//Se determina el pin de salida para el servo 
+ }
