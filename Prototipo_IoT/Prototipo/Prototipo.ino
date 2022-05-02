2ff85eb6ff2f227e594d37ab9463a520e167b538
Oscar Rodriguez <oscar.rodriguez02@correo.usa.edu.co>
2022-05-01 19:07:30 -0500

    Se definen las variables tanto de entradas como de salidas y se incluyen las librerias necesarias

--- Prototipo_IoT/Prototipo/Prototipo.ino
+++ Prototipo_IoT/Prototipo/Prototipo.ino
@@ -3,3 +3,10 @@ Integrantes:
 Diana Quintero
 Juan Rodriguez
 Oscar Rodriguez*/
+#include <Servo.h> //se incluye la libreria para la compuerta del aspersor
+Servo aspersor; //se define la variable aspersor
+int rojo=11; //se define el led rojo con el pin 11
+int verde=9; //se define el led verde con el pin 10
+int humedad=A0; //se define la humedad al analogico 0
+float tempC; // se define variable para almacenar el valor obtenido de temperatura
+int temp = A1; // se define variable del pin de entrada del sensor (A1)
